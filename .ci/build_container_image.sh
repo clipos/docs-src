03c1d20a5fa176b61c8581cd17288b6a8feb9b68
Timothée Ravier <timothee.ravier@ssi.gouv.fr>
2020-01-13 09:30:27 +0100

    ci: build container image as unprivileged user
    Change-Id: I4589b62da9265b1088f1be7a0c3e36dda5a814e8
    Signed-off-by: Timothée Ravier <timothee.ravier@ssi.gouv.fr>

--- .ci/build_container_image.sh
+++ .ci/build_container_image.sh
@@ -4,12 +4,8 @@
 
 set -o errexit -o nounset -o xtrace -o pipefail
 
-export STORAGE_DRIVER=vfs
+# Create Docker compatible images
 export BUILDAH_FORMAT=docker
-
-sed -i -e 's|^mount_program|#mount_program|g' /etc/containers/storage.conf
-sed -i -e 's|^mountopt|#mountopt|g' /etc/containers/storage.conf
-sed -i -e '/\/var\/lib\/shared/d' /etc/containers/storage.conf
 
 TIMESTAMP="$(date '+%Y%m%d%H%M%S')"
 LOCAL_NAME="doc:${TIMESTAMP}"
@@ -20,3 +16,5 @@ buildah login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 
 buildah push ${LOCAL_NAME} ${CI_REGISTRY_IMAGE}:${TIMESTAMP}
 buildah push ${LOCAL_NAME} ${CI_REGISTRY_IMAGE}:latest
+
+buildah rmi ${LOCAL_NAME}
